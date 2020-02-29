c47f72d965f465ef0d5235dd03075827cb9d356b
Victor Hugo <victormantova@gmail.com>
2020-02-29 01:14:28 -0300

    init repo

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() productx;
+  @Output() notify = new EventEmitter();
+
+  ngOnInit() {
+  }
+
+}
